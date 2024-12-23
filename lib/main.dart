6dd7dafe1199e348ee8a7915be7bb5af07db4a0f
mohit_s <mohit.gandhi@supersourcing.com>
2024-12-23 23:14:06 +0530

    doc hover fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 /// Entry point of the application.
@@ -25,6 +27,8 @@ class MyApp extends StatelessWidget {
                 Icons.call,
                 Icons.camera,
                 Icons.photo,
+                Icons.ice_skating_rounded,
+                Icons.import_contacts
               ],
             ),
           ),
@@ -34,7 +38,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// A customizable MacOS-like dock with draggable and reorderable buttons.
 class MacOSDock extends StatefulWidget {
   const MacOSDock({
     super.key,
@@ -49,24 +52,27 @@ class MacOSDock extends StatefulWidget {
 }
 
 class _MacOSDockState extends State<MacOSDock> with SingleTickerProviderStateMixin {
-  late ValueNotifier<List<IconData>> _dockItemsNotifier;
+  late ValueNotifier<List<IconData>> dockItemsNotifier;
+  late ValueNotifier<int?> hoveredIndexNotifier;
 
   @override
   void initState() {
     super.initState();
-    _dockItemsNotifier = ValueNotifier(List.from(widget.items));
+    dockItemsNotifier = ValueNotifier(List.from(widget.items));
+    hoveredIndexNotifier = ValueNotifier<int?>(null);
   }
 
   @override
   void dispose() {
-    _dockItemsNotifier.dispose();
+    dockItemsNotifier.dispose();
+    hoveredIndexNotifier.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
+      height: 90,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         color: Colors.black.withOpacity(0.5),
@@ -74,7 +80,7 @@ class _MacOSDockState extends State<MacOSDock> with SingleTickerProviderStateMix
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       margin: const EdgeInsets.only(bottom: 20),
       child: ValueListenableBuilder<List<IconData>>(
-        valueListenable: _dockItemsNotifier,
+        valueListenable: dockItemsNotifier,
         builder: (context, dockItems, _) {
           return Row(
             mainAxisSize: MainAxisSize.min,
@@ -87,17 +93,27 @@ class _MacOSDockState extends State<MacOSDock> with SingleTickerProviderStateMix
                   final newIndex = index;
                   updatedDockItems.removeAt(oldIndex);
                   updatedDockItems.insert(newIndex, details.data);
-                  _dockItemsNotifier.value = updatedDockItems;
+                  dockItemsNotifier.value = updatedDockItems;
                 },
                 builder: (context, candidateData, rejectedData) {
-                  return Draggable<IconData>(
-                    data: dockItems[index],
-                    feedback: Transform.scale(
-                      scale: 1.2,
-                      child: DockItem(icon: dockItems[index], isDragging: true),
+                  return MouseRegion(
+                    onEnter: (_) => hoveredIndexNotifier.value = index,
+                    onExit: (_) => hoveredIndexNotifier.value = null,
+                    child: ValueListenableBuilder<int?>(
+                      valueListenable: hoveredIndexNotifier,
+                      builder: (context, hoveredIndex, _) {
+                        final isHovered = hoveredIndex == index;
+                        return Draggable<IconData>(
+                          data: dockItems[index],
+                          feedback: Transform.scale(
+                            scale: 1.2,
+                            child: DockItem(icon: dockItems[index], isDragging: true),
+                          ),
+                          childWhenDragging: const SizedBox.shrink(),
+                          child: DockItem(icon: dockItems[index], isHovered: isHovered),
+                        );
+                      },
                     ),
-                    childWhenDragging: const SizedBox.shrink(),
-                    child: DockItem(icon: dockItems[index]),
                   );
                 },
               );
@@ -114,21 +130,32 @@ class DockItem extends StatelessWidget {
   const DockItem({
     super.key,
     required this.icon,
+    this.isHovered = false,
     this.isDragging = false,
   });
 
   final IconData icon;
+  final bool isHovered;
   final bool isDragging;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: const Duration(milliseconds: 300),
-      constraints: const BoxConstraints(minWidth: 48, minHeight: 48),
+      constraints: BoxConstraints(
+        minWidth: isHovered ? 60 : 50,
+        minHeight: isHovered ? 60 : 50,
+        maxWidth: isHovered ? 60: 50,
+        maxHeight: isHovered ? 60 : 50,
+      ),
       margin: const EdgeInsets.symmetric(horizontal: 6),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
-        color: isDragging ? Colors.grey.shade800 : Colors.white,
+        color: isDragging
+            ? Colors.grey.shade800
+            : isHovered
+            ? Colors.grey.shade300
+            : Colors.white,
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.3),
@@ -141,10 +168,11 @@ class DockItem extends StatelessWidget {
       child: Center(
         child: Icon(
           icon,
-          color: isDragging ? Colors.white : Colors.black,
+          color: isDragging || isHovered ? Colors.black : Colors.grey.shade800,
           size: 32,
         ),
       ),
     );
   }
 }
+
